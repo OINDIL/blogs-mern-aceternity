5914de530020f6c4c93a70b334e927749cefe975
Oindil Golder <91556756+OINDIL@users.noreply.github.com>
2024-12-20 13:22:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Main/Home";
+import About from "./Main/About";
+import Contact from "./Main/Contact";
+import Blog from "./Main/Blog";
+import { Navbar } from "./Main/Navbar";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <>
+          <Navbar />
+          <Home />
+        </>
+      ),
+      errorElement: <div>404</div>,
+    },
+    {
+      path: "/about",
+      element: (
+        <>
+          <About />
+        </>
+      ),
+    },
+    {
+      path: "/contact",
+      element: (
+        <>
+          <Contact />
+        </>
+      ),
+    },
+    {
+      path: "/blog",
+      element: (
+        <>
+          <Blog />
+        </>
+      ),
+    },
+  ]);
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
