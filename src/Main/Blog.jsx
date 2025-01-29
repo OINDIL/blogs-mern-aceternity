69907bdc0cb4415993ca39588fb66620bb2ec71c
Oindil Golder <91556756+OINDIL@users.noreply.github.com>
2025-01-29 20:53:14 +0530

    all blogs frontend done

--- src/Main/Blog.jsx
+++ src/Main/Blog.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { useDataFlowContext } from "../Context/DataFlow";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import BlogsModal from "./blog-page-components/BlogsModal";
 const navItems = [
@@ -22,9 +21,53 @@ const navItems = [
 ];
 
 function Blog() {
-  const { darkMode, setDarkMode } = useDataFlowContext();
+  const [blogs, setBlogs] = useState({
+    author: "No author found",
+    allBlogs: [
+      {
+        title: "No title",
+        content: "No content",
+        createdAt: new Date(),
+      },
+    ],
+  });
 
+  const [refreshPage, setRefreshPage] = useState(false);
   const [openModal, setOpenModal] = useState(false);
+  const getUserBlogs = async () => {
+    try {
+      const res = await fetch("http://localhost:3000/blogs/all-blogs", {
+        method: "GET",
+        credentials: "include",
+      });
+
+      const data = await res.json();
+
+      if (data) {
+        setBlogs({
+          author: data.author,
+          allBlogs: data.blogs.map((blog) => {
+            return {
+              title: blog.title,
+              content: blog.content,
+              createdAt: blog.createdAt,
+            };
+          }),
+        });
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getUserBlogs();
+  }, []);
+
+  useEffect(() => {
+    getUserBlogs();
+  }, [refreshPage]);
+
   return (
     <>
       <section>
@@ -56,7 +99,51 @@ function Blog() {
             />
           </div>
         </nav>
-        <BlogsModal setOpenModal={setOpenModal} openModal={openModal} />
+        <BlogsModal
+          setOpenModal={setOpenModal}
+          openModal={openModal}
+          refreshPage={refreshPage}
+          setRefreshPage={setRefreshPage}
+        />
+      </section>
+
+      <section className="max-w-6xl mx-auto px-12 mt-4 space-y-4">
+        <div>
+          <h2 className="text-3xl font-semibold">Featured Blogs 📝</h2>
+          <p className="text-gray-600">
+            Some featured blogs from all the users
+          </p>
+          <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mt-4">
+            {blogs.allBlogs.map((blogs, index) => (
+              <div
+                key={index}
+                className={`border rounded p-2 ${
+                  index === 0 && `col-span-1 md:col-span-2`
+                }`}
+              >
+                <h3 className="text-xl font-medium">{blogs.title}</h3>
+                <p className="text-gray-600">{blogs.content}</p>
+              </div>
+            ))}
+          </main>
+        </div>
+        <div>
+          <h2 className="text-3xl font-semibold">All Blogs 📝</h2>
+          <p className="text-gray-600">Read some premium quality blogs</p>
+          <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mt-4">
+            {blogs.allBlogs.map((blogs, index) => (
+              <div
+                key={index}
+                className={`border rounded p-2 ${
+                  index === 0 && `col-span-1 md:col-span-2`
+                }`}
+              >
+                <h3 className="text-xl font-medium">{blogs.title}</h3>
+                <p className="text-gray-600">{blogs.content}</p>
+              </div>
+            ))}
+          </main>
+        </div>
       </section>
     </>
   );
--- src/Main/blog-page-components/BlogsModal.jsx
+++ src/Main/blog-page-components/BlogsModal.jsx
@@ -1,12 +1,87 @@
 import { Button, Modal } from "flowbite-react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
-export default function BlogsModal({ openModal, setOpenModal }) {
+export default function BlogsModal({
+  openModal,
+  setOpenModal,
+  refreshPage,
+  setRefreshPage,
+}) {
+  const [toast, setToast] = useState({
+    success: false,
+    error: false,
+  });
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleBlogSubmit = async () => {
+    try {
+      if (!title || !content) {
+        return alert("Please enter title and content");
+      }
+
+      const body = {
+        title,
+        content,
+      };
+
+      const res = await fetch("http://localhost:3000/blogs/create", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        setToast({
+          error: true,
+        });
+      }
+
+      setToast({
+        success: true,
+      });
+
+      setRefreshPage(!refreshPage);
+
+      console.log(data);
+    } catch (error) {
+      setToast({
+        error: true,
+      });
+    }
+  };
   return (
     <>
       <Modal show={openModal} onClose={() => setOpenModal(false)}>
         <Modal.Header>Creating blogs 😄🤩</Modal.Header>
         <Modal.Body>
           <div>
+            {toast.success && (
+              <div className="p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200">
+                <span className="flex gap-1">
+                  Blog created successfully.
+                  <Link to="/blogs" className="font-medium underline">
+                    View blog
+                  </Link>
+                </span>
+              </div>
+            )}
+
+            {toast.error && (
+              <div className="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200">
+                <span className="flex gap-1">
+                  Blog creation failed. Please try again.
+                </span>
+              </div>
+            )}
+
             <h2 className="text-xl font-bold text-gray-900 dark:text-white">
               Write your blogs
             </h2>
@@ -26,6 +101,7 @@ export default function BlogsModal({ openModal, setOpenModal }) {
                   className="dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500
                   rounded-lg bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                   placeholder="Mein toh raste se ja rha tha"
+                  onChange={(e) => setTitle(e.target.value)}
                 />
               </div>
 
@@ -40,15 +116,16 @@ export default function BlogsModal({ openModal, setOpenModal }) {
                   className="dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500
                   rounded-lg bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                   placeholder="Mein toh bhelpuri kha rha tha"
+                  onChange={(e) => setContent(e.target.value)}
                 />
               </div>
             </form>
           </div>
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={() => setOpenModal(false)}>Submit</Button>
+          <Button onClick={handleBlogSubmit}>Post</Button>
           <Button color="gray" onClick={() => setOpenModal(false)}>
-            Decline
+            Cancel
           </Button>
         </Modal.Footer>
       </Modal>
